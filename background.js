41ab70352f6a7b97c5bdb68e14c887160f0f36f4
Thomas van Nellen <tvnellen@hotmail.com>
2017-05-26 00:53:54 -0500

    Add unchecked icon

--- background.js
+++ background.js
@@ -1,16 +1,12 @@
 function getToggle(callback) {
   chrome.storage.local.get('toggle', function(data){
-    if(data.toggle === undefined) {
-      callback(true); // default value
-    } else {
-      callback(data.toggle);
-    }
+    callback(data.toggle);
   });
 }
 
 function setToggle(value, callback) {
   chrome.storage.local.set({toggle : value}, function(){
-    if(chrome.runtime.lastError) {
+    if (chrome.runtime.lastError) {
       throw Error(chrome.runtime.lastError);
     } else {
       callback();
@@ -20,13 +16,18 @@ function setToggle(value, callback) {
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  getToggle(function(toggle){
+  getToggle(function(toggle) {
     toggle = !toggle;
-    setToggle(toggle, function(){
+    setToggle(toggle, function() {
       // Send a message to the active tab
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {"check" : toggle});
+      });
+      // Toggle icon
+      newIcon = (toggle) ? "uncheck.png" : "check.png";
+      chrome.browserAction.setIcon({
+        path: newIcon
       });
     });
   });
