8d27926cafe5569b386404659e756dd88d84324d
Thomas van Nellen <tvnellen@hotmail.com>
2017-05-25 23:27:07 -0500

    Add toggle to allow unchecking

--- background.js
+++ background.js
@@ -1,8 +1,33 @@
+function getToggle(callback) {
+  chrome.storage.local.get('toggle', function(data){
+    if(data.toggle === undefined) {
+      callback(true); // default value
+    } else {
+      callback(data.toggle);
+    }
+  });
+}
+
+function setToggle(value, callback) {
+  chrome.storage.local.set({toggle : value}, function(){
+    if(chrome.runtime.lastError) {
+      throw Error(chrome.runtime.lastError);
+    } else {
+      callback();
+    }
+  });
+}
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  getToggle(function(toggle){
+    toggle = !toggle;
+    setToggle(toggle, function(){
+      // Send a message to the active tab
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"check" : toggle});
+      });
+    });
   });
 });
--- content.js
+++ content.js
@@ -1,11 +1,8 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var checkboxes = document.getElementsByTagName('input');
-
-      for (var i=0; i<checkboxes.length; i++) {
-        if (checkboxes[i].type == 'checkbox') checkboxes[i].checked = true;
-      }
+    var checkboxes = document.getElementsByTagName('input');
+    for (var i=0; i<checkboxes.length; i++) {
+      if (checkboxes[i].type == 'checkbox') checkboxes[i].checked = request.check;
     }
   }
 );
