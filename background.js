da2777973fc5550226960e7cd30a7b1646479e0b
Thomas van Nellen <tvnellen@hotmail.com>
2017-06-10 13:42:37 -0500

    Remember state for each tab

--- background.js
+++ background.js
@@ -1,34 +1,44 @@
-function getToggle(callback) {
-  chrome.storage.local.get('toggle', function(data){
-    callback(data.toggle);
-  });
-}
-
-function setToggle(value, callback) {
-  chrome.storage.local.set({toggle : value}, function(){
-    if (chrome.runtime.lastError) {
-      throw Error(chrome.runtime.lastError);
-    } else {
-      callback();
-    }
-  });
-}
+// Object that will hold state (checked/unchecked) for every tab
+var states = {};
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  getToggle(function(toggle) {
-    toggle = !toggle;
-    setToggle(toggle, function() {
-      // Send a message to the active tab
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"check" : toggle});
-      });
-      // Toggle icon
-      newIcon = (toggle) ? "uncheck.png" : "check.png";
-      chrome.browserAction.setIcon({
-        path: newIcon
-      });
-    });
+
+  // Update state
+  states[tab.id] = !states[tab.id];
+  
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"check" : states[tab.id]});
   });
+  // Toggle icon
+  setIcon(states[tab.id]);
+
 });
+
+// Listen for new tab to be activated
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  
+  if (!(activeInfo.tabId in states)) {
+    states[activeInfo.tabId] = false;
+  }
+  setIcon(states[activeInfo.tabId]);
+  
+});
+
+// Listen for current tab to be changed
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  
+  setIcon(states[tab.id]);
+  
+});
+
+function setIcon(state) {
+  
+  newIcon = (state) ? "uncheck.png" : "check.png";
+    chrome.browserAction.setIcon({
+    path: newIcon
+  });
+  
+}
